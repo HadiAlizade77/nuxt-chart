b87355d528d7d8857cfb686bfc7f4b015df80992
Hadi alizadeh <alizadehadi08@gmail.com>
2022-08-14 01:27:41 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <chart :chart-data="chartData" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data: () => ({
+    chartData: null,
+    chart: null,
+  }),
+  async fetch() {
+    this.chartData = await this.$axios.$get(
+      'https://europe-west2-nozzle.cloudfunctions.net/api'
+    )
+  },
+})
+</script>
